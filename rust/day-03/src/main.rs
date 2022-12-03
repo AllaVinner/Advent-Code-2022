4d4aaf314a7ff3f412738b5da4efbb737428ea89
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-04 00:36:38 +0100

    Rust solution day03 part 1

--- rust/day-03/src/main.rs
+++ rust/day-03/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     let file = fs::read_to_string("./input.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", part1::main(&file));
     //println!("Answer to part 2: {}", part2::main(&file));
+    //println!("{:?}", 'A' as i32); // prints 65
 }
--- rust/day-03/src/part1.rs
+++ rust/day-03/src/part1.rs
@@ -9,29 +9,29 @@ pub fn main(input: &str) -> String {
             get_priority(bag)
         })
         .sum::<i32>();
-    println!("{:?}", type_of(&result));
     result.to_string()
 }
 
 
 
 pub fn get_priority(bag: &str) -> i32 {
-    let half: i32 = bag.len() / 2;
-    let comp1 = &str[..half]
-    let comp2 = &str[half..] 
+    let half= bag.len() / 2;
+    let comp1 = &bag[..half];
+    let comp2 = &bag[half..];
+    let mut num: i32 =0;
     for item1 in comp1.chars() {
         for item2 in comp2.chars() {
             if item1 == item2 {
-                num = (item1 as i32)
+                num = item1 as i32;
                 if num > 90 {
                     return num -96;
                 } else {
                     return num - 65 + 27;
-                }
-                return 0;
-            }
+                };
+            };
         }
     }
+    return 0;
 }
 
 
