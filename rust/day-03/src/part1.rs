9a16288c8d2049b9b662b80e3cbdf8e402cb305a
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-03 16:41:59 +0100

    day 01 part 1

--- rust/day-03/src/part1.rs
+++ rust/day-03/src/part1.rs
@@ -4,6 +4,41 @@ fn type_of<T>(_: &T) -> &'static str {
 
 pub fn main(input: &str) -> String {
     let result = input
+        .lines()
+        .map(|bag| {
+            get_priority(bag)
+        })
+        .sum::<i32>();
     println!("{:?}", type_of(&result));
     result.to_string()
 }
+
+
+
+pub fn get_priority(bag: &str) -> i32 {
+    let half: i32 = bag.len() / 2;
+    let comp1 = &str[..half]
+    let comp2 = &str[half..] 
+    for item1 in comp1.chars() {
+        for item2 in comp2.chars() {
+            if item1 == item2 {
+                num = (item1 as i32)
+                if num > 90 {
+                    return num -96;
+                } else {
+                    return num - 65 + 27;
+                }
+                return 0;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
