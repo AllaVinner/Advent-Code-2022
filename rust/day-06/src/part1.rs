10541c6123781fd96d43f2b693368c119f4aae0f
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-06 13:24:12 +0100

    Refactoring

--- rust/day-06/src/part1.rs
+++ rust/day-06/src/part1.rs
@@ -8,10 +8,9 @@ pub fn main(input: &str) -> String {
     let mut char_iter = input.chars();
     let mut num_over_one: i32 = 0;
     let len: usize = 4;
-
-    let mut old_c: char;
     let mut c: char;    
-    for ii in 0..4 {
+    
+    for _ in 0..len {
         c = char_iter.next().unwrap();
         window.push_back(c);
         h.entry(c).and_modify(|v| *v += 1).or_insert(1 as i32);
@@ -20,28 +19,22 @@ pub fn main(input: &str) -> String {
         }
     }
     
-    let mut new_c: char;
     let mut i: usize = 4;
     while num_over_one != 0 {
-        old_c = window.pop_front().unwrap();
-        h.entry(old_c).and_modify(|v| *v -= 1);
-        if (*h.get(&old_c).unwrap() == 1) {
+        c = window.pop_front().unwrap();
+        h.entry(c).and_modify(|v| *v -= 1);
+        if (*h.get(&c).unwrap() == 1) {
             num_over_one -= 1;
         }
 
-        new_c = char_iter.next().unwrap();
-        window.push_back(new_c);
+        c = char_iter.next().unwrap();
+        window.push_back(c);
         
-        h.entry(new_c).and_modify(|v| *v += 1).or_insert(1 as i32);
-        if (*h.get(&new_c).unwrap() == 2) {
+        h.entry(c).and_modify(|v| *v += 1).or_insert(1 as i32);
+        if (*h.get(&c).unwrap() == 2) {
             num_over_one += 1;
         }
-
         i += 1;
     }
-    println!("{:?}", i);
-    
-
-    //  map.get(&'a').cloned().unwrap_or(0);
-    "AAA".to_string()
+    i.to_string()
 }
