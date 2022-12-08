eee762a9e791cd512b5e52f5fd13fe957e7174d8
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-08 21:19:30 +0100

    day08 fixed typo

--- rust/day-08/src/main.rs
+++ rust/day-08/src/main.rs
@@ -5,5 +5,5 @@ mod part2;
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", part1::main(&file));
-    println!("Answer to part 1: {}", part2::main(&file));
+    println!("Answer to part 2: {}", part2::main(&file));
 }
