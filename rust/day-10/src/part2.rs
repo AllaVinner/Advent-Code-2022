0cc42c67c7d2e933841ce73c76707ce6f98c275a
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-10 15:59:31 +0100

    Working day 10 part 2

---
+++ rust/day-10/src/part2.rs
@@ -0,0 +1,45 @@
+
+fn eval(cycles: i32, regestry: i32) {
+    if (regestry - (cycles % 40)).abs() <= 1 {
+        print!("#");
+    } else {
+        print!(".");
+    }     
+    if cycles % 40 == 0 {
+        println!("n");
+    }
+}
+
+pub fn main(input: &str) -> String {
+    let mut cycles: i32 = 1;
+    let mut regestry: i32 = 1;
+    let mut record: i32 = 0;
+    
+    let mut tmp_str;
+    let mut tmp_iter;
+
+    for line in input.lines() {
+        if line.starts_with("noop") {    
+            eval(cycles, regestry);
+            cycles += 1; 
+            continue;
+        }
+
+        eval(cycles, regestry);
+        cycles += 1;
+        tmp_iter = line.split(" ");
+        tmp_str = tmp_iter.next().unwrap();
+        tmp_str = tmp_iter.next().unwrap();
+
+        regestry += tmp_str.parse::<i32>().unwrap();
+        eval(cycles, regestry);
+        cycles += 1;
+
+    }
+    println!("{:?}", cycles);
+
+    record.to_string()
+}
+
+
+
