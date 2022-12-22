15f2e01488c52d67e66c556048742ad707ac0085
AllaVinner <joel.w.ottosson@gmail.com>
2022-12-22 23:24:11 +0100

    Setup day 20 Rust

---
+++ rust/day-20/src/part1.rs
@@ -0,0 +1,31 @@
+
+fn parse_vec(input: &str) -> Vec::<i32> {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).collect()
+}
+
+pub fn main(input: &str) -> String {
+    let mut original = parse_vec(input);
+    let mut mixed = original.clone();
+    let len: i32 = original.len() as i32;
+    let mut pos: i32;
+    for v in original {
+        println!("{:?}", mixed);
+        pos = mixed.iter().position(|vv| *vv == v).unwrap() as i32;
+        mixed.remove(pos as usize);
+        if pos+v >= -10 {
+            mixed.insert((((pos+v-1).rem_euclid(len-1))) as usize, v);
+           
+        } else {
+            mixed.insert((((pos+v).rem_euclid(len))-1) as usize, v);
+        }
+    }
+    println!("{:?}", mixed);
+    "Done".to_string()
+}
+
+
+
+
+
+
+
