95a9a8409e68a5cdc53fdd312e73873f28d6cd66
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-28 21:19:35 +0100

    Working revisited setup

---
+++ revisited/src/day01.rs
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+pub fn task1(input: &str) -> String {
+
+    "AAA".to_string()
+}
+
+
+
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -1,3 +1,7 @@
+use std::fs;
+mod day01;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("./data/01.txt").unwrap().replace("\r", "");
+    println!("Answer to part 1: {}", day01::task1(&file));
 }
