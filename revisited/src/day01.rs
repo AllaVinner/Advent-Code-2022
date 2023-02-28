257cc293dd00fcfccf4b92ef19c141bc237b124b
AllaVinner <joel.w.ottosson@gmail.com>
2023-02-28 21:31:24 +0100

    Day 1 task 1 done

--- revisited/src/day01.rs
+++ revisited/src/day01.rs
@@ -6,8 +6,15 @@
 
 
 pub fn task1(input: &str) -> String {
-
-    "AAA".to_string()
+    input.split("\n\n")
+        .map(|bag| 
+            bag.lines()
+               .map(|cal| cal.parse::<u32>().unwrap())
+               .sum::<u32>()
+        )
+        .max()
+        .unwrap()
+        .to_string()
 }
 
 
