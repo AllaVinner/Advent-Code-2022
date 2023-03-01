e1927785fccfbe7dfb54c5eb27023c54b4a4a6e3
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-01 20:22:26 +0100

    init day 2 revisited

---
+++ revisited/src/day02.rs
@@ -0,0 +1,24 @@
+
+
+
+
+
+pub fn task1(input: &str) -> String {
+    "aaa"to_string()
+}
+
+
+pub fn task2(input: &str) -> String {
+    "aaa".to_string()
+}
+
+
+
+
+
+
+
+
+
+
+
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
-mod day01;
+mod day02;
 
 fn main() {
-    let file = fs::read_to_string("./data/01.txt").unwrap().replace("\r", "");
-    println!("Answer to part 1: {}", day01::task1(&file));
-    println!("Answer to part 2: {}", day01::task2(&file));
+    let file = fs::read_to_string("./data/02.txt").unwrap().replace("\r", "");
+    println!("Answer to part 1: {}", day02::task1(&file));
+    println!("Answer to part 2: {}", day02::task2(&file));
 }
