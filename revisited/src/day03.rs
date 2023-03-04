9b2c54a7de2e48d95de6d6b1b8b03fa81cf46d63
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-04 13:01:32 +0100

    Eorking first part 3 task 1

--- revisited/src/day03.rs
+++ revisited/src/day03.rs
@@ -5,7 +5,12 @@
 
 
 pub fn task1(input: &str) -> String {
-    
+    let a = input.lines()
+        .map(|line| (line, line.chars().count()))
+        .map(|(line, len)| line.split_at(len / 2))
+        .map(|(bag1, bag2)| bag1.chars().find(|c1|  bag2.find(c1).is_some()).unwrap())
+        .collect();
+    println!("{:?}", a);
     "asdf".to_string()
 }
 
