7e46794c90b86f168141b5422732ad8f4ba9ff3b
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-04 13:31:39 +0100

    Working day 3 task3 revisited

--- revisited/src/day03.rs
+++ revisited/src/day03.rs
@@ -1,18 +1,23 @@
 
-
-
+fn parse_char(c: char) -> u32{
+    let ascii_num = c as u32;
+    if ascii_num >= 96 {
+        ascii_num - 96
+    } else {
+        ascii_num - 64 + 26
+    }
+}
 
 
 
 pub fn task1(input: &str) -> String {
-    let a: Vec<char> = input.lines()
+    input.lines()
         .map(|line| (line, line.chars().count()))
         .map(|(line, len)| line.split_at(len / 2))
         .map(|(bag1, bag2)| bag1.chars().find(|c1| bag2.find(*c1).is_some()).unwrap())
-        .take(5)
-        .collect();
-    println!("{:?}", a);
-    "asdf".to_string()
+        .map(|c| parse_char(c))
+        .sum::<u32>()
+        .to_string()
 }
 
 
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -4,7 +4,7 @@ use crate::day03::{task1, task2};
 mod day03;
 
 fn main() {
-    let file = fs::read_to_string("./data/02.txt").unwrap().replace("\r", "");
+    let file = fs::read_to_string("./data/03.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", task1(&file));
     println!("Answer to part 2: {}", task2(&file));
 }
