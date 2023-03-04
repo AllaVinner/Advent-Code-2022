c82378a48f53422b662a6cf1a043a7fc76ac0a62
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-04 13:06:33 +0100

    Working iterator to char

--- revisited/src/day03.rs
+++ revisited/src/day03.rs
@@ -5,10 +5,11 @@
 
 
 pub fn task1(input: &str) -> String {
-    let a = input.lines()
+    let a: Vec<char> = input.lines()
         .map(|line| (line, line.chars().count()))
         .map(|(line, len)| line.split_at(len / 2))
-        .map(|(bag1, bag2)| bag1.chars().find(|c1|  bag2.find(c1).is_some()).unwrap())
+        .map(|(bag1, bag2)| bag1.chars().find(|c1| bag2.find(*c1).is_some()).unwrap())
+        .take(5)
         .collect();
     println!("{:?}", a);
     "asdf".to_string()
