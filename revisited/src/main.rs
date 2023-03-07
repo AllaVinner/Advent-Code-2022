178cfb8a1e8b90c0c9071abe109e94d3ccd5e12e
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-07 19:38:23 +0100

    day 5 setup

--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
-use crate::day04::{task1, task2};
-mod day04;
+use crate::day05::{task1, task2};
+mod day05;
 
 fn main() {
-    let file = fs::read_to_string("./data/04.txt").unwrap().replace("\r", "");
+    let file = fs::read_to_string("./data/05.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", task1(&file));
     println!("Answer to part 2: {}", task2(&file));
 }
