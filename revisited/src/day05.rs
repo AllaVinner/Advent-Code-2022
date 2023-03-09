6b20cc4c845f656148b55be5a62bbdfa1dcbf8af
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-09 20:32:17 +0100

    Refactoring

--- revisited/src/day05.rs
+++ revisited/src/day05.rs
@@ -1,10 +1,10 @@
-use std::str::Chars;
+use std::{str::Chars, collections::HashMap};
 
-struct CargoValue<'a> {
+struct CargoIter<'a> {
     iter: Chars<'a>
 }
 
-impl<'a> Iterator for CargoValue<'a> {
+impl<'a> Iterator for CargoIter<'a> {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -16,27 +16,22 @@ impl<'a> Iterator for CargoValue<'a> {
 }
 
 trait CargoIterator {
-    fn cargo_value(&self) -> CargoValue<'_>;
+    fn cargo_value(&self) -> CargoIter<'_>;
 }
 
 impl CargoIterator for &str {
-    fn cargo_value(&self) -> CargoValue<'_> {
-        CargoValue {iter: self.chars()}
+    fn cargo_value(&self) -> CargoIter<'_> {
+        CargoIter {iter: self.chars()}
     }
 }
 
+struct Cargo {
+    cargo: HashMap<char, Vec<char>>
+}
 
 pub fn task1(input: &str) -> String {
     let (init_input, change_input) = input.split_once("\n\n").unwrap();
     
-    init_input.lines()
-        .rev()
-        .skip(1)
-
-        .step_by(4)
-        .inspect(|c| println!("{:?}", c))
-        .count();
-
     let mut a = vec![1,2,3];
     a.push(5);
     println!("{:?}", &a);
