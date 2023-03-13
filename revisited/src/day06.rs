0faf553f4387579e7203a4684fedd1c82722446f
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-13 18:45:16 +0100

    Working solution with tupel windwo

--- revisited/src/day06.rs
+++ revisited/src/day06.rs
@@ -1,11 +1,16 @@
 
-
+use std::ops::Add;
 use itertools::Itertools;
 
 pub fn task1(input: &str) -> String {
-    let a: Vec<char> = input.chars().tuple_windows().map(|(c0,c1,c2,c3)| c2).collect();
-    println!("{:?}", a);
-    "ASD".to_string()
+    input.chars()
+        .tuple_windows()
+        .enumerate()
+        .find(|(i, (c0,c1,c2,c3))| c0 != c1 && c0 != c2 && c0 != c3 && c1 != c2 && c1 != c3 && c2 != c3)
+        .unwrap()
+        .0
+        .add(4)
+        .to_string()
 }
 
 
