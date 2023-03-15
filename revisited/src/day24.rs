417c96135e2c48ac0fe45a9c73a7d1d9c9df9683
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-15 22:39:34 +0100

    Setting up types for day 24

--- revisited/src/day24.rs
+++ revisited/src/day24.rs
@@ -1,4 +1,28 @@
 
+type time = usize;
+
+enum Move {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    WAIT
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Pos {
+    x: usize,
+    y: usize
+}
+
+enum Reached {
+    NotReached,
+    Initial(time),
+    ReachedFrom(Pos)
+}
+
+
+
 pub fn task1(input: &str) -> String {
     "AAA".to_string()
 }
