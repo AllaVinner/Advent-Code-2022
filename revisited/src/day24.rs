424b07525509395ca396808653bb4444fbcabf3f
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-15 22:56:35 +0100

    Added psudo code

--- revisited/src/day24.rs
+++ revisited/src/day24.rs
@@ -1,6 +1,7 @@
 use ndarray::{Array2, Array3};
 
-type ReachMap = Array2<Reached>;
+type OriginMap = Array2<Option<Origin>>;
+type OriginStack = Vec<Origin>;
 type BlizzardWorld = Array3<bool>;
 type Time = usize;
 
@@ -12,22 +13,36 @@ enum Move {
     WAIT
 }
 
+struct Path {
+    moves: Vec<Move>,
+    origin: Origin,
+    pos: Pos
+}
+
 #[derive(Debug, Clone, Copy)]
 struct Pos {
     x: usize,
     y: usize
 }
 
-enum Reached {
-    NotReached,
+enum Origin {
     Initial(Time),
-    ReachedFrom(Pos)
+    From(Pos)
 }
 
 
 
 pub fn task1(input: &str) -> String {
+    // Read in Blizzard World
+    // Initiate Origin Stack
+
+    // from each origin, find all possible reaches (That are none taken), populate the next origin bucket.
+    // If target reached, stop.
+
+    // Calculate path
+    
     "AAA".to_string()
+
 }
 
 
