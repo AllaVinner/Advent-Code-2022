c0288f16d9646b0ee80a27858ce074536c855e6f
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-20 20:18:08 +0100

    Day 25 working with digits

--- revisited/src/day25.rs
+++ revisited/src/day25.rs
@@ -1,8 +1,21 @@
 
 
+fn parse_digit(c: char) -> isize {
+    match c {
+        '2' => 2,
+        '1' => 1,
+        '0' => 0,
+        '-' => -1,
+        '=' => -2,
+        _ => panic!("Got none expected character.")
+    }
+}
 
 pub fn task1(input: &str) -> String {
-    "AAA".to_string()
+    input.lines()
+        .map(|line| line.chars().fold(0, |n, c| 5*(n+parse_digit(c)) ))
+        .sum::<isize>()
+        .to_string()
 }
 
 
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -3,7 +3,7 @@ use crate::day25::{task1, task2};
 mod day25;
 
 fn main() {
-    let file = fs::read_to_string("./data/25.txt").unwrap().replace("\r", "");
+    let file = fs::read_to_string("./data/25_test.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", task1(&file));
     println!("Answer to part 2: {}", task2(&file));
 }
