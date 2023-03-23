4584d4aa91b51f0ab54903322d2136fe1da8cbc9
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-24 00:42:38 +0100

    parse metadat day 22

--- revisited/src/day22.rs
+++ revisited/src/day22.rs
@@ -1,10 +1,42 @@
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Pos {
+    x: i32,
+    y: i32
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Line {
+    start: u32,
+    len: u32
+}
 
 
+fn parse_input(input: &str) -> HashSet<Pos> {
+    input.lines()
+        .enumerate()
+        .map(|(y, line)| line.chars().enumerate().filter_map(|(x, c)| match c {
+            '#' => Some(Pos {x: x as i32, y: y as i32}),
+            _ => None
+        }).collect::<HashSet<Pos>>())
+        .flatten()
+        .collect::<HashSet<Pos>>()
+}
 
+fn parse_metadata(input: &str) {
+    let mut row_def: Vec<Line> = input.lines()
+        .enumerate()
+        .map(|(y, line)| {
+            let start = line.chars().position(|c| c != ' ').unwrap();
+            Line {start: start as u32, len: (line.chars().count() - start) as u32 }
+        })
+        .collect();
+        
+}
 
 
 pub fn task1(input: &str) -> String {
-
     "AAA".to_string()
 }
 
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
-use crate::day23::{task1, task2};
-mod day23;
+use crate::day22::{task1, task2};
+mod day22;
 
 fn main() {
-    let file = fs::read_to_string("./data/23.txt").unwrap().replace("\r", "");
+    let file = fs::read_to_string("./data/22.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", task1(&file));
     println!("Answer to part 2: {}", task2(&file));
 }
