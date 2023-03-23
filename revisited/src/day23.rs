a95c0d83f43784cb43ba52116a2c57d521604b11
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-23 21:20:23 +0100

    First proposal for task 1

--- revisited/src/day23.rs
+++ revisited/src/day23.rs
@@ -1,4 +1,4 @@
-
+use std::cmp::{min, max};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct Pos {
@@ -40,7 +40,7 @@ fn get_direction_neighbours(pos: &Pos, direction: Direction) -> [Pos; 3] {
         Direction::NORTH => [Pos{x: pos.x-1, y: pos.y-1 }, Pos{x: pos.x, y: pos.y-1}, Pos{x: pos.x+1, y: pos.y-1}],
         Direction::SOUTH => [Pos{x: pos.x-1, y: pos.y+1}, Pos{x: pos.x, y: pos.y+1}, Pos{x: pos.x+1, y: pos.y+1}],
         Direction::WEST => [Pos{x: pos.x-1, y: pos.y-1}, Pos{x: pos.x-1, y: pos.y}, Pos{x: pos.x-1, y: pos.y+1}],
-        Direction::EAST => [Pos{x: pos.x+1, y: pos.y-1}, Pos{x: pos.x+1, y: pos.y}, Pos{x: pos.x+1, y: pos.y-1}]
+        Direction::EAST => [Pos{x: pos.x+1, y: pos.y-1}, Pos{x: pos.x+1, y: pos.y}, Pos{x: pos.x+1, y: pos.y+1}]
     } 
 }
 
@@ -74,17 +74,43 @@ fn elf_proposal(pos: &Pos, elves: &Vec<Pos>, first_direction: Direction) -> Pos 
     return *pos;
 }
 
+fn get_extremes(elves: &Vec<Pos>) -> [i32; 4] {
+    let mut min_x = i32::MAX;
+    let mut max_x = i32::MIN;
+    let mut min_y = i32::MAX;
+    let mut max_y = i32::MIN;
+    for pos in elves.iter() {
+        min_x = min(min_x, pos.x);
+        max_x = max(max_x, pos.x);
+        min_y = min(min_y, pos.y);
+        max_y = max(max_y, pos.y);
+    }
+    [min_x, max_x, min_y, max_y]
+}
 
 
 pub fn task1(input: &str) -> String {
     let mut elves = parse_input(input);
-    let mut first_direction = get_direction_iterator(Direction::NORTH);
-    let mut proposals: Vec<Pos> = elves.iter().map(|elf| elf_proposal(elf, &elves, Direction::NORTH)).collect();
+    let mut proposals: Vec<Pos> = Vec::new();
+    for first_direction in get_direction_iterator(Direction::NORTH).take(10) {
+        proposals = elves.iter().map(|elf| elf_proposal(elf, &elves, first_direction)).collect();
+        for (pos, proposal) in elves.iter_mut().zip(proposals.iter()) {
+            if proposals.iter().filter(|p| *p == proposal).count() == 1 {
+                *pos = *proposal;
+            }
+        }
+    }
 
-
-    println!("{:?}", elves);
-    println!("{:?}", proposals);
-
+    let [min_x, max_x, min_y, max_y] = get_extremes(&elves);
+    let area = (max_x - min_x+1) * (max_y-min_y+1);
+    for p in elves.iter() {
+        println!("x: {}, y: {}", p.x, p.y);
+    }
+    
+    println!("{:?}", area);
+    println!("Num Elves {:?}", elves.len());
+    println!("Width {:?}", (max_x - min_x+1));
+    println!("Height {:?}", (max_y-min_y+1));
     "ASD".to_string()
 }
 
