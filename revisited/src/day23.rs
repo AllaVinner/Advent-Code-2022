0042a529bf7c6839a6e6ede0a539a7230f85f591
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-23 20:19:30 +0100

    Implemnted direction iterator

--- revisited/src/day23.rs
+++ revisited/src/day23.rs
@@ -1,17 +1,30 @@
 
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Pos {
     x: i32,
     y: i32
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     NORTH,
     SOUTH,
     WEST,
     EAST
 }
+
+
+fn get_direction_iterator(first_direction: Direction)  -> impl Iterator<Item = Direction> {
+    let mut m =  vec![Direction::NORTH, Direction::SOUTH, Direction::WEST, Direction::EAST];
+    m.into_iter().cycle().skip(match first_direction {
+        Direction::NORTH => 0,
+        Direction::SOUTH => 1,
+        Direction::WEST => 2,
+        Direction::EAST => 3
+    }).take(4)
+}
+
 
 fn parse_input(input: &str) -> Vec<Pos> {
     input.lines()
@@ -29,6 +42,7 @@ fn parse_input(input: &str) -> Vec<Pos> {
 pub fn task1(input: &str) -> String {
     let mut elves = parse_input(input);
     println!("{:?}", elves);
+
     "ASD".to_string()
 }
 
