4f4b9c423bf1f168f7f19a748aa8ba7365e9ac72
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-24 12:09:30 +0100

    Parsing complete day 22

--- revisited/src/day22.rs
+++ revisited/src/day22.rs
@@ -12,8 +12,28 @@ struct Line {
     len: u32
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum Rotation {
+    LEFT,
+    RIGHT
+}
 
-fn parse_input(input: &str) -> HashSet<Pos> {
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum Instruction {
+    RIGHT,
+    LEFT,
+    GO(u32)
+}
+
+impl Line {
+    fn inside(&self, i: u32) -> bool {
+        self.start <= i && i < self.start+self.len
+    }
+}
+
+
+fn parse_objects(input: &str) -> HashSet<Pos> {
     input.lines()
         .enumerate()
         .map(|(y, line)| line.chars().enumerate().filter_map(|(x, c)| match c {
@@ -31,22 +51,55 @@ fn parse_metadata(input: &str) -> (Vec<Line>, Vec<Line>) {
             Line {start: start as u32, len: (line.chars().count() - start) as u32 }
         })
         .collect();
-    let num_col = input.lines().next().unwrap().chars().count();
+
+    let num_col = input.lines().map(|line| line.chars().count()).max().unwrap();
     let mut col_def: Vec<Line> = Vec::with_capacity(num_col);
+
     let col_defs: Vec<Line> = (0..num_col).into_iter()
         .map(|col_i| {
-            let start = row_defs.iter().position(|rd| rd.start <= col_i as u32).unwrap();
-            let end = row_defs.iter().position(|rd| col_i as u32 <= rd.start + rd.len).unwrap();
+            let start = row_defs.iter().position(|rd| rd.inside(col_i as u32)).unwrap();
+            let end = row_defs.len() - row_defs.iter().rev().position(|rd| rd.inside(col_i as u32)).unwrap();
             Line {start: start as u32, len: (end-start) as u32}
         })
         .collect();
     (row_defs, col_defs)
 }
 
+fn parse_instructions(input: &str) -> Vec<Instruction> {
+    let mut current_num = 0;
+    let mut instructions = Vec::<Instruction>::new();
+    for c in input.chars() {
+        match c {
+            'R' => {
+                if current_num != 0 {
+                    instructions.push(Instruction::GO(current_num));
+                }
+                current_num = 0;
+                instructions.push(Instruction::RIGHT)
+            },
+            'L' => {
+                if current_num != 0 {
+                    instructions.push(Instruction::GO(current_num));
+                }
+                current_num = 0;
+                instructions.push(Instruction::LEFT)
+            },
+            n => {
+                current_num = 10*current_num + n.to_digit(10).unwrap();
+            }
+        }
+    }
+    instructions
+}
+
 
 pub fn task1(input: &str) -> String {
-    let md = parse_metadata(input);
-    println!("{:?}", md);
+    let (map_input, instruction_input) = input.split_once("\n\n").unwrap();
+    let objects = parse_objects(map_input);
+    let (row, col) = parse_metadata(map_input);
+    let instructions = parse_instructions(instruction_input);
+    println!("{:?}", col);
+    println!("{:?}", objects);
     "AAA".to_string()
 }
 
