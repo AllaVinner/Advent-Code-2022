482815f3e5981d2566d649ea1cce4cc2e1763a36
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-29 21:48:29 +0200

    Init day 20 revisited

---
+++ revisited/src/day20.rs
@@ -0,0 +1,13 @@
+
+
+pub fn task1(input: &str) -> String {
+    
+    "AAA".to_string()
+}
+
+
+
+pub fn task2(input: &str) -> String {
+    
+    "AAA".to_string()
+}
--- revisited/src/main.rs
+++ revisited/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
-use crate::day19::{task1, task2};
-mod day19;
+use crate::day20::{task1, task2};
+mod day20;
 
 fn main() {
-    let file = fs::read_to_string("./data/19_test.txt").unwrap().replace("\r", "");
+    let file = fs::read_to_string("./data/20_test.txt").unwrap().replace("\r", "");
     println!("Answer to part 1: {}", task1(&file));
     println!("Answer to part 2: {}", task2(&file));
 }
