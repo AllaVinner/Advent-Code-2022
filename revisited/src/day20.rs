5f0b55df33cbceac28d3475e40e70a312cacabf1
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-31 22:16:50 +0200

    Working test data example

--- revisited/src/day20.rs
+++ revisited/src/day20.rs
@@ -13,9 +13,18 @@ fn parse_input(input: &str) -> (VecDeque<i64>, VecDeque<i64>) {
 
 pub fn task1(input: &str) -> String {
     let (mut numbers, mut order) = parse_input(input);
-    println!("{:?}", numbers);
-    println!("{:?}", order);
-    "AAA".to_string()
+    let length = numbers.len();
+    for current_num in order.iter() {
+        let idx = numbers.iter().position(|v| v == current_num).unwrap();
+        let positive_value = ((current_num  % length as i64) + length as i64) as usize;
+        numbers.remove(idx);
+        println!("{:?} {:?} {:?}", idx, current_num, length);
+        numbers.insert((idx+positive_value) % length, *current_num);
+    }
+    let zero_pos = order.iter().position(|v| *v == 0_i64).unwrap();
+    numbers.rotate_left(zero_pos);
+    let n = numbers.get(1000 % numbers.len()).unwrap() + numbers.get(2000 % numbers.len()).unwrap() +numbers.get(3000 % numbers.len()).unwrap(); 
+    n.to_string()
 }
 
 
