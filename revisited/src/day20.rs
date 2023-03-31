368cf0b629e5fa3d35a51917800ccd139431498a
AllaVinner <joel.w.ottosson@gmail.com>
2023-03-31 21:56:10 +0200

    Fixed stupid reading mistake on day 20

--- revisited/src/day20.rs
+++ revisited/src/day20.rs
@@ -1,7 +1,20 @@
+use std::collections::VecDeque;
 
+
+fn parse_input(input: &str) -> (VecDeque<i64>, VecDeque<i64>) {
+    let mut numbers = VecDeque::<i64>::new();
+    let mut order = VecDeque::<i64>::new();
+    for line in input.lines() {
+        numbers.push_back(line.parse().unwrap());
+        order.push_back(line.parse().unwrap());
+    }
+    (numbers, order)
+}
 
 pub fn task1(input: &str) -> String {
-    
+    let (mut numbers, mut order) = parse_input(input);
+    println!("{:?}", numbers);
+    println!("{:?}", order);
     "AAA".to_string()
 }
 
