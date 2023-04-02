fe818f86e40104963e7ff3780b1e63caebe2dff0
AllaVinner <joel.w.ottosson@gmail.com>
2023-04-02 08:22:38 +0200

    Refactored test solution

--- revisited/src/day20.rs
+++ revisited/src/day20.rs
@@ -16,7 +16,7 @@ pub fn task1(input: &str) -> String {
     let length = numbers.len()-1;
     for current_num in order.iter() {
         //println!("{:?}", numbers);
-        let idx = numbers.iter().position(|v| v == current_num).unwrap() % length;
+        let idx = numbers.iter().position(|v| v == current_num).unwrap() % (length+1);
         numbers.remove(idx);
         //let positive_value = (current_num  + numbers.len() as i64) as usize;
         let positive_value = ((current_num  % length as i64) + length as i64) as usize;
@@ -25,12 +25,13 @@ pub fn task1(input: &str) -> String {
 
     let zero_pos = numbers.iter().position(|v| *v == 0_i64).unwrap();
     numbers.rotate_left(zero_pos);
-    let n = numbers.get(1000 %  numbers.len()).unwrap() + numbers.get(2000 %  numbers.len()).unwrap() +numbers.get(3000 % numbers.len()).unwrap(); 
+    let n = numbers.get(1000).unwrap() 
+               + numbers.get(2000 ).unwrap() 
+               + numbers.get(3000 ).unwrap(); 
     
     
     n.to_string()
 }
-
 
 
 pub fn task2(input: &str) -> String {
