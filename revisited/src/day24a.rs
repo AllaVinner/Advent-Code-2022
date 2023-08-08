5beac4e6ccb9a61d814a41c3d3aa28e5f8854c7b
AllaVinner <joel.w.ottosson@gmail.com>
2023-08-08 22:27:23 +0200

    added new attempt for 24a

---
+++ revisited/src/day24a.rs
@@ -0,0 +1,16 @@
+
+
+
+pub fn task1(input: &str) -> String {
+    println!("Hell");
+    "SD".to_string()
+}
+
+
+pub fn task2(input: &str) -> String {
+    println!("Hell");
+    "SD".to_string()
+}
+
+
+
