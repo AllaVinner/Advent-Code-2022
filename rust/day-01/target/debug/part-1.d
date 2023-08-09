84b227e5453a7ad57d1315b0b2a13bfd304a05ac
AllaVinner <joel.w.ottosson@gmail.com>
2023-08-09 20:58:59 +0200

    Cleaning up

--- rust/day-01/target/debug/part-1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\joelw\OneDrive\Documents\Projects\Programming\Advent-Code-2022\rust\day-01\target\debug\part-1.exe: C:\Users\joelw\OneDrive\Documents\Projects\Programming\Advent-Code-2022\rust\day-01\src\bin\part-1.rs C:\Users\joelw\OneDrive\Documents\Projects\Programming\Advent-Code-2022\rust\day-01\src\lib.rs
