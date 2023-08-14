02e06537b913b5f00d0ec2d0ccbe4aa909e8158e
AllaVinner <joel.w.ottosson@gmail.com>
2023-08-14 22:21:28 +0200

    Started parsing day 19 input

--- revisited/src/day19a.rs
+++ revisited/src/day19a.rs
@@ -1,7 +1,25 @@
+use std::collections::HashSet;
+
+
+struct Cost {
+    unit: String,
+    amount: u32
+}
+
+struct Factory {
+    product: String,
+    cost: Vec<Cost>
+}
 
 
 
 
+type Products = HashSet<String>;
+
+fn parse_input(input: &str) -> Vec<(Products, Vec<Factory>)> {
+    input.lines()
+        .
+} 
 
 
 
